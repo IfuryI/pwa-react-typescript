7f31701f8a85c41e00475aa7537330e233fc5506
wrongangel <wrongangel@gmail.com>
2023-03-17 11:26:32 +0300

    - pets questionnaire basic

--- src/pages/Profile/BasicQuestions/Pets/Pets.tsx
+++ src/pages/Profile/BasicQuestions/Pets/Pets.tsx
@@ -1,6 +1,36 @@
+import { Box, Button, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { useActive } from 'src/components/ProgressSlider/ProgressSlider'
+
 const Pets: React.FunctionComponent = () => {
+  const navigate = useNavigate()
+  const { setActive, setPercent } = useActive()
   return (
-    <div>Pets</div>
+    <>
+      <Typography variant='h1'>Do you have pets?</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <Box sx={{ flexGrow: '1' }}>
+          text
+        </Box>
+        <Box sx={{ gap: '.5rem', display: 'flex' }}>
+          <Button variant='text'
+          sx={{ flex: '1' }}
+          onClick={() => {
+            setPercent(50, 'pets')
+          }}>
+            50 Percent
+          </Button>
+          <Button variant='contained'
+            sx={{ color: 'white', flex: '1' }}
+            onClick={() => {
+              navigate('/profile/questionnaire-basic-info/smoking')
+              setActive('smoking')
+            }}>
+            Next
+          </Button>
+        </Box>
+      </Box>
+    </>
   )
 }
 export default Pets
