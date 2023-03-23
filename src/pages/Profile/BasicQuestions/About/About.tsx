a3cc146354dfb8d21f67e66b8e8e8e6714162024
wrongangel <wrongangel@gmail.com>
2023-03-23 13:13:26 +0300

    Basic Questions About page

--- src/pages/Profile/BasicQuestions/About/About.tsx
+++ src/pages/Profile/BasicQuestions/About/About.tsx
@@ -1,6 +1,55 @@
+import { Box, Button, styled, TextField, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useActive } from "src/components/ProgressSlider/ProgressSlider"
+import { useBasicQuestions } from "src/layouts/QuestionnaireBasic/QuestionnaireBasic"
+import styles from '../BasicQuestions.module.scss'
+
 const About: React.FunctionComponent = () => {
+  const { setActive, setPercent, setPercentAndGo } = useActive()
+  const { questions, setQuestions } = useBasicQuestions()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    questions.about ?? '' ? setPercent(1, 1, 'about') : setPercent(0, 1, 'about')
+  }, [questions])
+
   return (
-    <div>About</div>
+    <Box className={styles.question}>
+      <Box className={styles.question__head}>
+        <Typography className={styles.question__head_text} variant='h1'>Something else you think will be helpfull?</Typography>
+      </Box>
+      <Box className={styles.question__content}>
+        <TextField
+          value={questions.about}
+          onChange={(e) => {setQuestions({ ...questions, about: e.target.value })}}
+          hiddenLabel
+          fullWidth
+          multiline
+          size='small'
+          rows={21}
+        />
+      </Box>
+      <Box className={styles.question__nav}>
+        <Button variant='text'
+          fullWidth
+          onClick={() => {
+            setQuestions({ ...questions, about: '' })
+            setPercentAndGo(0, 1, 'about', 'contacts')
+            navigate('/profile/questionnaire-basic-info/contacts')
+          }}>
+          Skip
+        </Button>
+        <Button variant='contained'
+          fullWidth
+          onClick={() => {
+            navigate('/profile/questionnaire-basic-info/contacts')
+            setActive('contacts')
+          }}>
+          Next
+        </Button>
+      </Box>
+    </Box>
   )
 }
 export default About
